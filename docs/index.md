12a28c21d7dc872c2b14e003946fa2a42c5f6f0d
asudau <sudau@elan-ev.de>
2018-11-20 16:29:04 +0100

    alten kram raus

--- docs/index.md
+++
@@ -1,5 +0,0 @@
-﻿(M)OOC.IP
-=========
-
-Dies ist die (wachsende) Anleitung für (M)OOC.IP und Courseware.
-
--- models/mooc/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Mooc;
-
-const PLUGIN_DISPLAY_NAME_ID      = 'MOOC_PLUGIN_DISPLAY_NAME';
-
-// Field scopes
-const SCOPE_BLOCK = 'block_id-null';
-const SCOPE_USER  = 'block_id-user_id';
